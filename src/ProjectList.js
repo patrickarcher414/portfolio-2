51b896839a7aa1564ae269cc6bdc9800c5b77215
patrickarcher414 <patrickarcher414@gmail.com>
2022-10-03 11:03:11 -0500

    added ProjectList

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,7 +1,34 @@
 const ProjectList = ()=> {
+  
+  const projects = [
+    {
+      name: 'coding-n-kobolds',
+      image: '../assets/projects/coding-n-kobolds.PNG',
+      alt: 'coding-n-kobolds app demo',
+      deployedLink: 'https://codingnkobolds.herokuapp.com/',
+      githubLink: 'https://github.com/woodb58/coding-n-kobolds',
+    },
+    {
+      name: 'gather-the-magic',
+      image: '../assets/projects/gather-the-magic.PNG',
+      alt: 'gather-the-magic app demo',
+      deployedLink: 'https://gather-the-magic.herokuapp.com/',
+      githubLink: 'https://github.com/patrickarcher414/gather-the-magic',
+    },
+    {
+      name: 'meal-maker',
+      image: '../assets/projects/meal-maker.PNG',
+      alt: 'meal-maker app demo',
+      deployedLink: 'https://patrickarcher414.github.io/meal-maker/',
+      githubLink: 'https://github.com/patrickarcher414/meal-maker',
+    },
+  ]
+
+  const projectList = projects.map((project)=> 
+      <li>{project}</li>
+    )
+  
   return (
-    <div className="project-list">
-      
-    </div>
+    <ul>{projectList}</ul>
   )
-}
+};
