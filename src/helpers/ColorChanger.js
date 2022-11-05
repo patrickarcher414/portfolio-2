59f3bbbbaea00038d5ce3e202b742465e666bf2c
patrickarcher414 <patrickarcher414@gmail.com>
2022-11-05 10:33:13 -0500

    working on getting and setting the date value

---
+++ src/helpers/ColorChanger.js
@@ -0,0 +1,23 @@
+
+import React, {useState} from "react";
+
+// update the value of total days passed in the current year (1-365)
+const setDateVal = (dateVal)=> {
+
+  new Date;
+  let dateVal = 0
+
+}
+
+// calculate the total number of days passed in the current year (1-365)
+const getDateVal = ()=> {
+  new Date;
+  let dateVal = Date.now();
+  return dateVal
+}
+
+
+const colorGreen = ()=> {
+
+}
+
